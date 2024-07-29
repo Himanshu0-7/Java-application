ae1b52507433da6aaeb6b1fb8a0c974b9f040e04
Himanshu0-7 <167356709+Himanshu0-7@users.noreply.github.com>
2024-07-29 11:57:26 +0530

    Delete CreateInvoice directory

--- CreateInvoice/target/run.bat
+++
@@ -1,1 +0,0 @@
-java --module-path "C:\Users\himan\Downloads\openjfx-22.0.1_windows-x64_bin-sdk\javafx-sdk-22.0.1\lib" --add-modules javafx.controls,javafx.fxml -jar CreateInvoice-0.0.1-SNAPSHOT.jar
